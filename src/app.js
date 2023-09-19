a9fc6fef163f468760235264f919dbfe015ede2e
giaroligisel <giaroligisel@gmail.com>
2023-09-19 20:20:54 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app=express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
+});
+
+app.get("/form", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'views/form.html'))
+});
+
+
+app.get("/register", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'))
+});
+
+app.get("/login", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'))
+});
+
+app.listen(port, ()=> {
+    console.log('Servidor en http://localhost:' + port);
+});
